43a69733b62e778d04ef805314e6a9254e860206
dwight taylor <dtaylo37@calpoly.edu>
2025-01-14 17:11:33 -0800

    Second commit of my React app

---
+++ main.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+import "./main.css";
+
+function MyApp() {
+	  return (
+		      <// src/main.jsx
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+import "./main.css";
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+    </div>
+  );
+}
+
+// Create the container
+const container = document.getElementById("root");
+
+// Create a root
+const root = ReactDOMClient.createRoot(container);
+
+// Initial render: Render an element to the Root
+root.render(<MyApp />);div>
+		        <h1>Hello, React!</h1>
+		      </div>
+		    );
+}
+
+// Create the container
+// const container = document.getElementById("root");
+//
+// // Create a root
+// const root = ReactDOMClient.createRoot(container);
+//
+// // Initial render: Render an element to the Root
+// root.render(<MyApp />);// src/main.jsx
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+import "./main.css";
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+    </div>
+  );
+}
+
+// Create the container
+const container = document.getElementById("root");
+
+// Create a root
+const root = ReactDOMClient.createRoot(container);
+
+// Initial render: Render an element to the Root
+root.render(<MyApp />);// src/main.jsx
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+import "./main.css";
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+    </div>
+  );
+}
+
+// Create the container
+const container = document.getElementById("root");
+
+// Create a root
+const root = ReactDOMClient.createRoot(container);
+
+// Initial render: Render an element to the Root
+root.render(<MyApp />);
