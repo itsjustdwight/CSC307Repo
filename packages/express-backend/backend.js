a726a294fe8c0f037cb9716dc9cddd4c97130bc0
dwight taylor <dtaylo37@calpoly.edu>
2025-01-17 21:59:02 -0800

    6th commit to my React app

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,24 +1,60 @@
 // backend.js
+// creating express framework, creating a web server, and manging http requests and responses
 import express from "express"
 
-// creating an instance of Express
+// creating an instance of Express application (used for routes, middleware, and server behavior)
 const app = express();
 // port number to remain constant to listen to incoming HTTP requests 
 const port = 8000;
 
-// used to process incoming data in JSON format
+// creating users object with a users_list array where each object contains an id, name, and job
+const users = {
+    users_list: [
+        {
+            id: "xyz789",
+            name: "Charlie",
+            job: "Janitor"
+        },
+        {
+            id: "abc123",
+            name: "Mac",
+            job: "Bouncer"
+        },
+        {
+            id: "ppp222",
+            name: "Mac",
+            job: "Professor"
+        },
+        {
+            id: "yat999",
+            name: "Dee",
+            job: "Aspiring actress"
+        },
+        {
+            id: "zap555",
+            name: "Dennis",
+            job: "Bartender"
+        },
+    ]
+};
+
+// adding middleware to be used to process incoming HTTP requests in JSON format
 app.use(express.json());
 
 // sets endpoint to accept http requests
-// the "/" is the URL pattern that'll map to this endpoint
+// the "/" is the route for the GET HTTP method on root path
 // (req, res) is the callback function with a request then a response
-app.get("/", (req, res) => { 
-    res.send("Hello World!");
+// when request is made to root URL (http://localhost:8000/), server responds with message
+// using "/users" makes the new URL -> http://localhost:8000/users, accessing the users variable
+app.get("/users", (req, res) => { 
+    res.send(users);
 });
 
+// starting server on specified port
 // listening (from the backend) to the incoming http requests on the port
+// confirmation message (console.log()) send to console to indicate server is listening
 app.listen(port, () => {
     console.log(
-        `Example app listening at http://localhost:${port}`
+        `Example app listening at http://localhost:${port}/users`
     );
 });
